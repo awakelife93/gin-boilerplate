0d408f06a7da71c51cd18be50bc9d6a7c0e59bb4
HyunwooPark <awakelife93@gmail.com>
2022-09-08 12:13:58 +0900

    init 함수 추가

--- main.go
+++ main.go
@@ -2,22 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/awakelife93/gin-boilerplate/src/lib"
 	"github.com/awakelife93/gin-boilerplate/src/lib/middleware"
 	"github.com/awakelife93/gin-boilerplate/src/lib/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-// todo: connect neo4j
+func loadEnvironmentFile() {
+	// * enter the env file name of your project.
+	error := godotenv.Load(".env")
+
+	if error != nil {
+		log.Fatal(error.Error())
+	}
+}
+
+// * pre loading application level env
+func init() {
+	loadEnvironmentFile()
+}
+
 func main() {
+	// * ReleaseMode (release) or DebugMode (debug)
+	// * default DebugMode (debug)
+	var mode string = os.Getenv("env")
 
-	fmt.Println("create engine")
-	var engine *gin.Engine = lib.CreateEngine()
+	if mode == "" {
+		mode = gin.DebugMode
+	}
 
-	fmt.Println("initialize router")
+	var engine *gin.Engine = lib.CreateEngine(mode)
 	engine = routes.Initialize(engine)
-
 	engine = middleware.Initialize(engine)
 
 	fmt.Println("Start Gin Application")
