9c59f1af41b73174fb99d0570e091a12fa9ead2b
HyunwooPark <awakelife93@gmail.com>
2022-10-27 10:17:56 +0900

    Product model Price omitempty option 추가

--- src/models/product.go
+++ src/models/product.go
@@ -7,5 +7,5 @@ import (
 type Product struct {
 	gorm.Model
 	Name  string `gorm:"column:name; type:varchar(255); not null" json:"product_name"`
-	Price int    `gorm:"column:price; not null" json:"product_price"`
+	Price int    `gorm:"column:price; not null" json:"product_price, omitempty"`
 }
